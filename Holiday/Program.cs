a60a48a71c1b067249acba7e8022a1e8009be015
L Stephens <lstephens46@my.stlcc.edu>
2019-04-11 14:51:43 -0500

    finish and pass all tests

--- Holiday/Program.cs
+++ Holiday/Program.cs
@@ -4,11 +4,26 @@ namespace Holiday
 {
     public static class Program
     {
+        
         public static void Main()
         {
+            DateTime leaving = DateTime.Today;
+            //int daysGone = 7;
+            DayReturning(leaving, 8);
             Console.ReadLine();
         }
 
+
         // TODO: Create a method that takes the day you are leaving on vacation and how many days you will be gone and return the name of the day of the week you will return.
+        //((int)DayOfWeek.Monday - (int)DateTime.Today.DayOfWeek + 7) % 7) + 1);
+        public static DayOfWeek DayReturning(DateTime leaving, int daysGone)
+        {
+
+            DayOfWeek dayReturning = leaving.AddDays(daysGone).DayOfWeek;
+            
+            Console.WriteLine(dayReturning);
+            return dayReturning;
+        }
+
     }
 }
--- Test/ProgramTest.cs
+++ Test/ProgramTest.cs
@@ -11,6 +11,7 @@ namespace Test
         {
             object result = Program.DayReturning(DateTime.Today, (((int)DayOfWeek.Monday - (int)DateTime.Today.DayOfWeek + 7) % 7) + 1);
 
+
             Assert.Equal("Tuesday", result.ToString());
         }
     }
